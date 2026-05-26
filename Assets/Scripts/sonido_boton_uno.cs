7389ece56d87f51803b1e740512686969cc3d7d5
alejandroescalantevaldivieso <alejandroescalantevaldivieso1@gmail.com>
2026-05-26 04:25:08 -0500

    feat: Integracion de menu principal

---
+++ Assets/Scripts/sonido_boton_uno.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sonido_boton_uno : MonoBehaviour
+{
+
+    public AudioSource sonido_boton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void sonido()
+    {
+        sonido_boton.Play();
+    }
+
+
+}
