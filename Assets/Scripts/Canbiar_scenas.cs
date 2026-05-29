3c60b7324e71055989d3724f5908a88de58fdec1
alejandroescalantevaldivieso <alejandroescalantevaldivieso1@gmail.com>
2026-05-28 21:22:23 -0500

    feat: Integracion de la primera scena y animacion de personaje

---
+++ Assets/Scripts/Canbiar_scenas.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Canbiar_scenas : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void cambiar_scena_02()
+    {
+        SceneManager.LoadScene("Scena02");
+    }
+}
---
+++ Assets/Scripts/cambiar_canvas.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cambiar_canvas : MonoBehaviour
+{
+    public GameObject cnv_menu_principal;
+    public GameObject cnv_seleccion_personaje;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ocultar_menu_principal()
+    {
+        cnv_menu_principal.SetActive(false);
+        cnv_seleccion_personaje.SetActive(true);
+    }
+    public void ocultar_seleccion_personaje()
+    {
+        cnv_seleccion_personaje.SetActive(false);
+        cnv_menu_principal.SetActive(true);
+    }
+}
